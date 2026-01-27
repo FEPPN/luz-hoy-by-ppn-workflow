2711871cbb5e9e6967fe127311fc21efab349ecd
PPN <christophe.mauger29@gmail.com>
2026-01-27 09:57:22 +0100

    Add script.py to analyze price data from API

---
+++ script.py
@@ -0,0 +1,39 @@
+import requests
+from datetime import date, timedelta
+url = "https://pyicrgwkyiqbrlcoyqkh.supabase.co/rest/v1/prices"
+headers = {
+   ACCESS KEYS TO PUT 
+}
+aujourdhui = date.today().strftime('%Y-%m-%d')
+demain = (date.today() + timedelta(days=1)).strftime('%Y-%m-%d')
+
+for jour in [aujourdhui, demain]:
+    print(f"\n--- ANALYSE DU {jour} ---")
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params={
+            "select": "*",
+            "day": "eq." + jour
+        }
+    )
+
+    data = response.json()
+
+    if data:
+        resultat = data[0]
+        
+        for p in resultat['prices']:
+            if p['best'] == True:
+                print(f"L'heure la moins chère est : {p['hour']} ({p['price_eur_per_kwh']} €)")
+                
+        prix_min = resultat['summary']['min']
+        prix_max = resultat['summary']['max']
+        prix_moy = resultat['summary']['avg']
+
+        print(f"Prix le plus bas : {prix_min} €/kWh")
+        print(f"Prix le plus haut : {prix_max} €/kWh")
+        print(f"Moyenne du jour : {prix_moy} €/kWh")
+    else:
+        print(f"Pas encore de données disponibles pour le {jour}.")
